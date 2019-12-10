11e43e301425759ddfb37c9a8bd53f8bf611a8b4
Juan Ariza <35510324+juancoariza@users.noreply.github.com>
2019-12-10 16:46:31 -0500

    Final Update - 12/10/19
    Adds Keyboard control to drone script

--- Final Project/Scripts/Pilot_Script.py
+++ Final Project/Scripts/Pilot_Script.py
@@ -155,6 +155,69 @@ class Drone(object):
 
     def flush(self):
         self.vehicle.flush() # closes it all
+        
+          
+ #-- Define the function for sending mavlink velocity command in body frame
+def set_velocity_body(vehicle, vx, vy, vz):
+    """ Remember: vz is positive downward!!!
+    http://ardupilot.org/dev/docs/copter-commands-in-guided-mode.html
+    
+    Bitmask to indicate which dimensions should be ignored by the vehicle 
+    (a value of 0b0000000000000000 or 0b0000001000000000 indicates that 
+    none of the setpoint dimensions should be ignored). Mapping: 
+    bit 1: x,  bit 2: y,  bit 3: z, 
+    bit 4: vx, bit 5: vy, bit 6: vz, 
+    bit 7: ax, bit 8: ay, bit 9:
+    """
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+            0,
+            0, 0,
+            mavutil.mavlink.MAV_FRAME_LOCAL_NED,
+            0b0000111111000111, #-- BITMASK -> Consider only the velocities
+            0, 0, 0,        #-- POSITION
+            vx, vy, vz,     #-- VELOCITY
+            0, 0, 0,        #-- ACCELERATIONS
+            0, 0)
+    vehicle.send_mavlink(msg)
+    #vehicle.flush()
+    
+    
+#-- Key event function
+def key(event):
+    if event.char == event.keysym: #-- standard keys
+        if event.keysym == 'r':
+            print("r pressed >> Set the vehicle to RTL")
+            vehicle.mode = VehicleMode("RTL")	
+		    
+    else: #-- non standard keys
+        if event.keysym == 'W':
+			#print("UP pressed >> moving forward")
+            set_velocity_body(vehicle, gnd_speed, 0, 0)
+        elif event.keysym == 'S':
+			#print("DOWN pressed >> moving backward")
+            set_velocity_body(vehicle,-gnd_speed, 0, 0)
+        elif event.keysym == 'A':
+			#print("LEFT pressed >> moving left")
+            set_velocity_body(vehicle, 0, -gnd_speed, 0)
+        elif event.keysym == 'D':
+			#print("RIGHT pressed >> moving right")
+            set_velocity_body(vehicle, 0, gnd_speed, 0)
+        elif event.keysym == 'I':
+			#print("UP pressed >> moving upward")
+            set_velocity_body(vehicle, 0, 0, gnd_speed)
+        elif event.keysym == 'J':
+			#print("DOWN pressed >> moving upward")
+            set_velocity_body(vehicle, 0, 0, -gnd_speed)
+    
+#---- MAIN FUNCTION
+#- Takeoff
+arm_and_takeoff(10)
+ 
+#- Read the keyboard with tkinter
+root = tk.Tk()
+print(">> Control the drone with the arrow keys. Press r for RTL mode")
+root.bind_all('<Key>', key)
+root.mainloop()
 
 
 def main():
